60e032d9518b49a017bbecc570f74b1a4ef9c308
Rosetta <135234996+gisellerosetta@users.noreply.github.com>
2025-07-04 09:42:47 +0100

    move fixture

--- gptables/test/test_gptable.py
+++ gptables/test/test_gptable.py
@@ -39,24 +39,6 @@ invalid_text_elements_incl_none.append(None)
 def does_not_raise():
     yield
 
-
-@pytest.fixture(scope="function")
-def create_gptable_with_kwargs():
-
-    def generate_gptable(format_dict=None):
-        base_gptable = {
-            "table": pd.DataFrame(),
-            "table_name": "table_name",
-            "title": "",
-            "index_columns": {},  # Override default, as no columns in table
-        }
-        if format_dict is not None:
-            base_gptable.update(format_dict)
-        return GPTable(**base_gptable)
-
-    return generate_gptable
-
-
 def test_init_defaults(create_gptable_with_kwargs):
     """
     Test that given a minimal input, default attributes are correct types.
